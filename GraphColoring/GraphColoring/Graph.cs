e15fef7877ef42ab5836b40215f6cf70e1c7a7ef
naporalukasz <naporalukasz92@gmail.com>
2014-11-21 01:32:25 +0100

    New class
    Add new class for Graph and Vertex

---
+++ GraphColoring/GraphColoring/Graph.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GraphColoring
+{
+    class Graph
+    {
+        public List<Vertex> vertex;
+
+        public List<Vertex> wVertex
+        {
+            get { return vertex; }
+            set { vertex = value; }
+        }
+
+        public Graph(List<Vertex> vertex)
+        {
+            this.vertex = vertex;
+        }
+
+        public static Graph ReadGraph(string path)
+        {
+            List<Vertex> nGraph = new List<Vertex>();
+            String line;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        nGraph.Add(new Vertex(creatNeighbours(line)));
+                    }
+                    sr.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Nie udało sie odczytać pliku :(");
+                Console.WriteLine(e.Message);
+            }
+            return new Graph(nGraph);
+        }
+
+        public static void WriteGraph(string path, Graph graph)
+        {
+            List<Vertex> nGraph = new List<Vertex>();
+            String line;
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    foreach (var el in graph.vertex)
+                    {
+                        line = "Color: " + el.Color.ToString() + "; Neighbour: ";
+                        foreach (var elm in el.Neighbours)
+                            line += elm.ToString() + ",";
+                        sw.WriteLine(line.TrimEnd(','));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Nie udało sie zapisać pliku :(");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static List<int> creatNeighbours(String neighbours)
+        {
+            List<int> tmp = new List<int>();
+            string[] value = neighbours.Split(';');
+            foreach (string el in value)
+                tmp.Add(Int32.Parse(el));
+            return tmp;
+        }
+    }
+}
---
+++ GraphColoring/GraphColoring/Vertex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphColoring
+{
+    class Vertex
+    {
+        int color;
+        List<int> neighbours;
+
+        public int Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        public List<int> Neighbours
+        {
+            get { return neighbours; }
+            set { neighbours = value; }
+        }
+
+        public Vertex(List<int> neighbours)
+        {
+            this.color = 0;
+            this.neighbours = neighbours;
+        }
+
+    }
+}
