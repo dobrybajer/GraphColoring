349b4b8a5c4e757ef3de70031d505650fcd05fbe
dobrybajer <kamil.zakk@gmail.com>
2014-12-29 04:05:46 +0100

    Version 2 of GPU - still doesnt work

--- GraphColoring/GraphColoringGPU/kernel.cu
+++ GraphColoring/GraphColoringGPU/kernel.cu
@@ -23,6 +23,7 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 #pragma region Headers
 
 cudaError_t runCuda(int*, int*, int, int);
+cudaError_t runCuda2(int*, int*, int*, int, int);
 cudaError_t initIndepSet(int, int*, int, int*, int, int, int*, int*, int*, int, int, int);
 int* BuildingIndependentSetsGPU(int N, int* Vertices, int* Offest, int verticesLength);
 
@@ -317,10 +318,10 @@ int main()
 {
 	Graph graph = ReadGraph("test.txt");
 
-	int roz = 1 << graph.n;
+	//int roz = 1 << graph.n;
 
 	//int* independentSet = BuildingIndependentSets(graph.n, graph.vertices, graph.neighbors);
-	int* independentSet = BuildingIndependentSetsGPU(graph.n, graph.vertices, graph.neighbors, graph.allVerticesCount);
+	//int* independentSet = BuildingIndependentSetsGPU(graph.n, graph.vertices, graph.neighbors, graph.allVerticesCount);
 	//cout << endl;
 	//for (int i = 0; i <roz; i++)
 	//{
@@ -329,7 +330,8 @@ int main()
 	//cout << endl;
 	int* tabWyn = new int[graph.n];
 
-	cudaError_t cudaStatus = runCuda(tabWyn, independentSet, graph.n, roz);
+	//cudaError_t cudaStatus = runCuda(tabWyn, independentSet, graph.n, roz);
+	cudaError_t cudaStatus = runCuda2(tabWyn, graph.vertices, graph.neighbors, graph.n, graph.allVerticesCount);
    
 	if (cudaStatus != cudaSuccess) 
 	{
@@ -436,9 +438,9 @@ cudaError_t initIndepSet(int N, int* Vertices, int verticeslength, int* Offset, 
   
 	
 	IndependentSetGPU<<<1,actualVerticesRowCount>>>( N , dev_Vertices, dev_Offset , actualVerticesRowCount,
-		 actualVerticesColCount,dev_actualVertices, dev_newVertices,dev_independentSets, col, el);
+		 actualVerticesColCount,dev_actualVertices, dev_newVertices, dev_independentSets, col, el);
 	
-	//cudaThreadSynchronize();
+	cudaThreadSynchronize();
 
 	  gpuErrchk(cudaGetLastError());
       
@@ -463,3 +465,149 @@ cudaError_t initIndepSet(int N, int* Vertices, int verticeslength, int* Offset, 
 }
 
 #pragma endregion CudaFunctions
+
+#pragma region CudaFunctions - version 2
+
+__global__ void Init1(int* independentSet, int* actualVertices, int verticesCount)
+{
+	for (int i = 0; i < verticesCount; ++i)
+	{
+		independentSet[1 << i] = 1;
+		actualVertices[i] = i;
+	}
+}
+
+__global__ void Init2(int* actualVertices, int* newVertices)
+{
+	actualVertices = newVertices;
+}
+
+__global__ void Init3(int* actualVertices, int* l_set, int n, int actualVerticesRowCount, int actualVerticesColCount)
+{
+	int last_el = 0;
+	l_set[0] = 0;
+	for(int i = 1; i < actualVerticesRowCount; ++i)
+	{
+		int j = n - actualVertices[(i - 1) * actualVerticesColCount + actualVerticesColCount - 1] - 1;
+	
+		if(j <= 0)
+			l_set[i] = -1;
+		else
+		{
+			l_set[i] = last_el + j;
+			last_el = j;
+		}
+	}
+}
+
+__global__ void IndependentSetGPU2(int* l_set, int n, int* Vertices, int* Offset, int actualVerticesRowCount, int actualVerticesColCount, int* actualVertices, int* newVertices, int* independentSets, int col, int el)
+{
+	int i = threadIdx.x;
+	int l = l_set[i];
+
+	if (l==-1) return;
+
+	int lastIndex = 0;
+	// Sprawdzenie indeksu poporzedniego zbioru dla rozpatrywanego podzbioru
+	for (int index = 0; index < actualVerticesColCount; ++index)
+		lastIndex += (1 << actualVertices[i * actualVerticesColCount + index]);
+
+	for (int j = actualVertices[i * actualVerticesColCount + actualVerticesColCount - 1] + 1; j < n; ++j)
+	{
+		int lastIndex2 = lastIndex;
+		// Sprawdzenie indeksu poprzedniego zbioru dla rozpatrywanego podzbioru \ {i}
+		for (int ns = Offset[j - 1]; ns < Offset[j]; ++ns)
+		{
+			for (int q = 0; q < actualVerticesColCount; ++q)
+			{
+				if (actualVertices[i * actualVerticesColCount + q] == Vertices[ns])
+				{
+					lastIndex2 -= (1 << Vertices[ns]);
+					break;
+				}
+			}		
+		}
+		int nextIndex = lastIndex + (1 << j);
+		// Liczba zbiorów niezale¿nych w aktualnie przetwarzanym podzbiorze
+		independentSets[nextIndex] = independentSets[lastIndex] + independentSets[lastIndex2] + 1;
+		for (int k = 0; k < el; ++k)
+			newVertices[l*col + k] = actualVertices[i * actualVerticesColCount + k];
+		newVertices[l * col + el] = j;
+				
+		l++;
+	}
+}
+
+cudaError_t runCuda2(int* wynik, int* vertices, int* offset, int verticesCount, int allVerticesCount)
+{
+    int* dev_vertices = 0;
+	int* dev_offset = 0;
+    int* dev_wynik = 0;
+
+	int* dev_independentSet = 0;
+	int* dev_actualVertices = 0;
+	int* dev_newVertices = 0;
+	int* dev_l_set = 0;
+	int actualVerticesRowCount = verticesCount;
+	int actualVerticesColCount = 1;
+	int PowerNumber = 1 << verticesCount;
+
+    cudaError_t cudaStatus = cudaSuccess;
+
+    gpuErrchk(cudaSetDevice(0));
+
+	gpuErrchk(cudaMalloc((void**)&dev_vertices, allVerticesCount * sizeof(int)));
+    gpuErrchk(cudaMalloc((void**)&dev_offset, verticesCount * sizeof(int)));
+    gpuErrchk(cudaMalloc((void**)&dev_wynik, verticesCount * sizeof(int)));
+
+	gpuErrchk(cudaMalloc((void**)&dev_independentSet, PowerNumber * sizeof(int)));
+	gpuErrchk(cudaMalloc((void**)&dev_actualVertices, verticesCount * sizeof(int)));
+
+    gpuErrchk(cudaMemcpy(dev_vertices, vertices, allVerticesCount * sizeof(int), cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMemcpy(dev_offset, offset, verticesCount * sizeof(int), cudaMemcpyHostToDevice));
+    
+	Init1<<<1,1>>> (dev_independentSet, dev_actualVertices, verticesCount);
+
+	for (int el = 1; el < verticesCount; el++)
+	{	
+		int col = el + 1;
+		int row = Combination_n_of_k(verticesCount, col);
+
+		gpuErrchk(cudaMalloc((void**)&dev_newVertices, (row * col) * sizeof(int)));
+		gpuErrchk(cudaMalloc((void**)&dev_l_set, actualVerticesRowCount * sizeof(int)));
+		
+		Init3<<<1,1>>> (dev_actualVertices, dev_l_set, verticesCount, actualVerticesRowCount, actualVerticesColCount);
+
+		int* l_set = new int[actualVerticesRowCount];
+		gpuErrchk(cudaMemcpy(l_set, dev_l_set, actualVerticesRowCount * sizeof(int), cudaMemcpyDeviceToHost));
+		cout << "l_set"<<endl;
+		for(int r = 0; r < actualVerticesRowCount; ++r)
+			cout << l_set[r] << " ";
+		cout << endl;
+
+
+
+		IndependentSetGPU2<<<1,actualVerticesRowCount>>> (dev_l_set, verticesCount, dev_vertices, dev_offset, actualVerticesRowCount, actualVerticesColCount, dev_actualVertices, dev_newVertices, dev_independentSet, col, el);
+	
+		cudaThreadSynchronize();
+
+		cudaFree(dev_actualVertices);
+		Init2<<<1,1>>> (dev_actualVertices, dev_newVertices);
+		
+		actualVerticesRowCount = row;
+		actualVerticesColCount = col;
+		cout<<"nr "<<el<<endl;
+	}
+
+	FindChromaticNumber<<<1,verticesCount>>>(verticesCount, dev_independentSet, dev_wynik);
+
+    gpuErrchk(cudaGetLastError());
+    
+    gpuErrchk(cudaDeviceSynchronize());
+
+    gpuErrchk(cudaMemcpy(wynik, dev_wynik, verticesCount * sizeof(int), cudaMemcpyDeviceToHost));
+
+    return cudaStatus;
+}
+
+#pragma endregion CudaFunctions - version 2
